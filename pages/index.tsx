c159540a197f4dc8be3427146e99e86c6a723c49
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2023-04-17 21:40:24 -0600

    basic qr code generator

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,177 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useEffect, useRef, useState } from "react";
+import QrCode from "qrcode";
+import { Pacifico } from "next/font/google";
+import {
+  TbCircleChevronDown,
+  TbCircleChevronUp,
+  TbDownload,
+} from "react-icons/tb";
+import { BlockPicker } from "react-color";
 
-const inter = Inter({ subsets: ['latin'] })
+const pacifico = Pacifico({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
+  const [qrData, setQrData] = useState<string>("https://cameronlucas.dev");
+  const [logoFile, setLogoFile] = useState<Blob>();
+  const [qrColor, setQrColor] = useState<string>("#555555");
+  const [bgColor, setBgColor] = useState<string>("#fff");
+
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [showAdvancedOptions, setShowAdvancedOptions] = useState<boolean>(true);
+
+  const generateQrCode = (data: string) =>
+    QrCode.toCanvas(canvas.current, data, {
+      errorCorrectionLevel: "high",
+      width: 300,
+      margin: 1,
+      version: 10,
+      color: {
+        dark: qrColor,
+        light: bgColor,
+      },
+    });
+
+  const handleQrCodeGeneration = () => {
+    const context = canvas.current?.getContext("2d");
+
+    if (!logoFile) return generateQrCode(qrData);
+
+    if (context) {
+      const img = new Image();
+      img.onload = () => {
+        if (canvas.current) {
+          context.clearRect(0, 0, canvas.current.width, canvas.current.height);
+          generateQrCode(qrData);
+          context.drawImage(
+            img,
+            canvas.current.width / 2 - 64,
+            canvas.current.height / 2 - 64,
+            128,
+            128
+          );
+        }
+      };
+      img.src = URL.createObjectURL(logoFile as any);
+    }
+  };
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQrData(e.target.value);
+  };
+
+  const handleLogoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLogoFile(e.target.files?.[0]);
+  };
+
+  const downloadQr = () => {
+    const imageData = canvas.current?.toDataURL("image/png");
+
+    if (imageData) {
+      const link = document.createElement("a");
+      link.download = "qrcode.png";
+      link.href = imageData;
+      link.click();
+    }
+  };
+
+  useEffect(() => {
+    generateQrCode(qrData);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="w-full min-h-screen bg-slate-100 flex flex-col justify-between items-center">
+      <div className="navbar bg-white">
+        <a className="normal-case text-xl">qrNexus</a>
+      </div>
+      <div className="flex sm:flex-row my-12 flex-col p-4 mx-4 w-full max-w-5xl card bg-white">
+        <figure className="flex-col sm:w-4/12">
+          <h3
+            className={`font-light text-4xl ${pacifico.className} text-blue-500`}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Scan Me
+          </h3>
+          <canvas ref={canvas} className="bg-white" />
+          <button
+            onClick={downloadQr}
+            className="btn w-11/12 my-2 bg-green-500 border-green-500 text-white hover:bg-transparent hover:text-green-500"
+          >
+            Download <TbDownload className="mx-2" />
+          </button>
+        </figure>
+        <div className="card-body sm:w-8/12">
+          <h1 className="font-light text-4xl mx-2 my-2 normal-case">qrNexus</h1>
+          <p className="font-extralight"></p>
+          <div className="flex sm:flex-nowrap flex-wrap w-full justify-center items-center">
+            <input
+              type="text"
+              placeholder="Website URL..."
+              value={qrData}
+              onChange={handleUrlChange}
+              className="input input-bordered w-full max-w-xl bg-transparent"
             />
-          </a>
+            <button
+              onClick={handleQrCodeGeneration}
+              className="btn w-full mt-2 sm:mt-0 sm:w-auto btn-outline hover:bg-blue-500 hover:text-white border-blue-500 text-blue-500 mx-2"
+            >
+              Generate QR Code
+            </button>
+          </div>
+          <span
+            className="cursor-pointer text-blue-500 self-end my-2"
+            onClick={() => setShowAdvancedOptions(!showAdvancedOptions)}
+          >
+            Advanced Options{" "}
+            {showAdvancedOptions ? (
+              <TbCircleChevronUp className="inline-block" />
+            ) : (
+              <TbCircleChevronDown className="inline-block" />
+            )}
+          </span>
+          <div
+            className={`ease-in-out duration-300 overflow-hidden ${
+              showAdvancedOptions ? "h-auto" : "h-0"
+            } flex-col`}
+          >
+            <div className="w-full flex items-center">
+              <span className="mr-2">
+                Logo/Image:
+                <br />
+                <span className="text-xs font-extralight">128px by 128px</span>
+              </span>
+              <input
+                type="file"
+                placeholder="Logo/Image Url"
+                className="file-input file-input-bordered w-full bg-transparent"
+                onChange={handleLogoChange}
+              />
+            </div>
+            <div className="flex flex-col sm:flex-row w-full justify-center sm:justify-between">
+              <div className="flex flex-col mt-6 items-center">
+                <span className="text-center">QR Code Color</span>
+                <BlockPicker
+                  color={qrColor}
+                  onChangeComplete={(color) => setQrColor(color.hex)}
+                  className="mt-4"
+                />
+              </div>
+              <div className="flex flex-col mt-6 items-center">
+                <span className="text-center">Background Color</span>
+                <BlockPicker
+                  color={bgColor}
+                  onChangeComplete={(color) => setBgColor(color.hex)}
+                  className="mt-4"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
+      <span className="mb-4">
+        Made with <span className="text-red-500">♥️</span> by{" "}
+        <a href="https://cameronlucas.dev" target="_blank" rel="noreferrer">
+          Cameron Lucas
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </span>
+    </div>
+  );
 }
